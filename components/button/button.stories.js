985b0f79c70a541fc9f4c640a9a5deca17e087c6
Tobias Ljungström <tobias.ljungstrom@gmail.com>
2021-01-26 17:16:28 +0100

    Adjust configuration of storybook installation

---
+++ components/button/button.stories.js
@@ -0,0 +1,5 @@
+export default {
+  title: "Example/Button",
+};
+
+export const Primary = () => `<mac-button></mac-button>`;
--- stories/Button.stories.js
+++
@@ -1,9 +0,0 @@
-import "../components/button";
-
-export default {
-  title: "Button",
-};
-
-export const Primary = () => {
-  return `<mac-button></mac-button>`;
-};
